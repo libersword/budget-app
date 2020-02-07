84eb3c1e207cb3dbbe426ff818ab3e44382f4e72
Lindsey Brown <libersword@gmail.com>
2020-02-07 16:23:00 -0700

    Mongo DB Stuff

--- server.js
+++ server.js
@@ -12,10 +12,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
